6fc41dc3e51eef921193527586a4357c4ca28004
wassup-999 <sebasquispeperez@gmail.com>
2026-03-25 19:05:14 -0500

    Avance

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -2,15 +2,38 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject Prefabobstacle;
+    public BoxCollider obstacleCollider;   
+    public int RiseValue;
+    public float DetectRange = 3;
+    public Transform Enemy;
+
+    public bool isNear = false;
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
+    }
     void Update()
     {
         
     }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            isNear = true;         
+            if(DetectRange >= Vector3.Distance(transform.position, Enemy.position))
+            {
+                DetectEnemyNear();
+            }
+
+        }        
+    }
+    public void DetectEnemyNear()
+    {
+         Prefabobstacle.transform.position = new Vector3(transform.position.x, transform.position.y + RiseValue, transform.position.z);
+    }
+    
+
 }
